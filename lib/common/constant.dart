3643247c1ad0315d0a4c76c30a7acfb1770e01ce
w568w <1278297578@qq.com>
2025-03-29 12:00:14 +0800

    change: use bank-specific .apk download URL

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -327,7 +327,7 @@ class Constant {
       return "https://apps.apple.com/app/id$APPSTORE_APPID";
     }
     if (PlatformX.isAndroid) {
-      return "https://static.fduhole.com/danxi-latest.apk";
+      return "https://danta.fudan.edu.cn/files/danta-ib-latest.apk";
     }
     return "https://danxi.fduhole.com";
   }
